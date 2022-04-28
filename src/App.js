898f9019da5ebc06e4f06992811aaa0ae90e2b65
Cole <cebCole@gmail.com>
2022-04-28 04:43:21 -0500

    work on multiple carousels of projects

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import Header from "./components/Header";
 import Project from "./components/Project";
 import Carousel from "./components/Carousel";
 
-
+//https://www.youtube.com/watch?v=6c2NqDyxppU react in django
 export default function App() {
 
     const [portfolioData, setPortfolioData] = React.useState([{}])
     const [project, setProject] = React.useState({})
+    const [techData, setTechData] = React.useState([{}])
+
+    let techUrl = "https://cole.pythonanywhere.com/api/v1/technologies/"
+    useEffect(() => {
+        fetch(techUrl)
+            .then(res => res.json())
+            .then(data => setTechData(data))
+            // .then(data => console.log(data))
+            // .then(data => setProject(data))
+    }, [techUrl])
 
     
 
@@ -31,6 +41,8 @@ export default function App() {
     
     }
 
+    console.log(portfolioData)
+    console.log(techData)
 
     return (
         
@@ -41,6 +53,7 @@ export default function App() {
   
 
             <hr></hr>
+            <h5>All Projects</h5>
             <section className="cards-list">
                 <Carousel data={portfolioData} handleClick={handleClick}/>  
             </section>
---
+++ src/components/MovieCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function MovieCard(props) {
+    let data = props.data
+    return (
+        <div className="project">
+            <div className="project--info">
+                <h1>{data.title}</h1>
+                <p>{data.description}</p>
+            </div>
+            <div className="image--div">
+                <img src={data.image} className="project--image"/>
+            </div>
+        </div>
+    )
+}
