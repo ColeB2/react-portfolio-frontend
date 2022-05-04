3a20c12fd0ecbd4b107f613031310fdd0b61c700
Cole <cebCole@gmail.com>
2022-05-04 07:13:53 -0500

    link back to simple version of portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,14 @@ import React from "react";
 export default function Header() {
     return (
         <header className="portfolio--header">
-            <h1>Cole B</h1>
+            <h1 className="name">Cole B</h1>
+            <h2 className="link">
+                <a 
+                    href="https://coleb.pythonanywhere.com/portfolio"
+                >
+                    Simple
+                </a>
+            </h2>
         </header>
 
     )
